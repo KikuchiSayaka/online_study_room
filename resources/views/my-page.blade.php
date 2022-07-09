0871ee291f6b47e52793f873b03fd4d73584621e
SayakaKikuchi <heperutravagliu@gmail.com>
2022-07-09 19:19:54 +0900

    Fix display of study time list on /my-page #3

--- resources/views/my-page.blade.php
+++ resources/views/my-page.blade.php
@@ -4,14 +4,24 @@
 
     <div class="container border-flame w-9 py-sm-5 py-3 my-5">
         <h2 class="text-center">勉強記録</h3>
-
-        @foreach ($records as $record)
-            <dl class="d-flex border-bottom p-1">
-                <dt class="p-1">{{ $record->updated_at }}</dt>
-                <dd class="p-1">勉強内容：{{ $record->learning_content }}</dd>
-                <dd class="p-1">勉強時間： {{ $record->total_minutes }}分</dd>
-            </dl>
-        @endforeach
+        <table class="table">
+            <thead class="main-bg-color">
+                <tr>
+                <th scope="col text-center">日付</th>
+                <th scope="col">勉強時間</th>
+                <th scope="col">勉強内容</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($records as $record)
+                    <tr>
+                        <th scope="row">{{ $record->updated_at }}</th>
+                        <td>{{ $record->total_minutes }}分</td>
+                        <td>{{ $record->learning_content }}</td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
     </div>
 
 @endsection
