84dc4c7de9267681e495af8ca8c46e3ce85f721c
SayakaKikuchi <heperutravagliu@gmail.com>
2022-07-16 15:30:30 +0900

    update Japanese version #13

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -5,20 +5,20 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Verify Your Email Address') }}</div>
+                <div class="card-header">{{ __('メールアドレスの確認') }}</div>
 
                 <div class="card-body">
                     @if (session('resent'))
                         <div class="alert alert-success" role="alert">
-                            {{ __('A fresh verification link has been sent to your email address.') }}
+                            {{ __('入力いただいたメールアドレスへ、認証メールを送信しました') }}
                         </div>
                     @endif
 
-                    {{ __('Before proceeding, please check your email for a verification link.') }}
-                    {{ __('If you did not receive the email') }},
+                    {{ __('お送りしたメール内にある確認リンクを押して、会員登録を完了してください。') }}
+                    {{ __('メールが届いていない方') }},
                     <form class="d-inline" method="POST" action="{{ route('verification.resend') }}">
                         @csrf
-                        <button type="submit" class="btn btn-link p-0 m-0 align-baseline">{{ __('click here to request another') }}</button>.
+                        <button type="submit" class="btn btn-link p-0 m-0 align-baseline">{{ __('こちらをクリックして、運営へお問い合わせください') }}</button>.
                     </form>
                 </div>
             </div>
