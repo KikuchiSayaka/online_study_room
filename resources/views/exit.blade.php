ee6e98e2f6e0ac603792dc1d5189d3e13481e345
SayakaKikuchi <heperutravagliu@gmail.com>
2022-07-17 13:35:46 +0900

    fix exit page display #17

--- resources/views/exit.blade.php
+++ resources/views/exit.blade.php
@@ -6,12 +6,11 @@
             <div class="border-flame w-sm-50 m-auto bg-white exit-board">
                 <dl class="total-time text-center mt-3">
                     <dt>総勉強時間</dt>
-                    <dd>60:00</dd>
+                    <dd>{{ $user->total_minutes }}分</dd>
                 </dl>
                 <h3 class="text-center fs-4">勉強内容</h3>
                 <div class="m-auto w-25 text-center mb-5">
-                    <p class="m-0">PHP</p>
-                    <p class="m-0">Laravel</p>
+                    <p class="m-0">{{ $user->learning_content }}</p>
                 </div>
             </div>
             <p class="text-center mt-4">勉強した記録にツイートしてみませんか？</p>
